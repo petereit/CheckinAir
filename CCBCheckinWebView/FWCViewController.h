03dcc672faa51336bb98da5eb7979bf9d936f7f7
Mark Petereit <MarkPetereit@Mark-Petereits-iMac.local>
2014-07-18 12:17:54 -0400

    Initial Commit

---
+++ CCBCheckinWebView/FWCViewController.h
@@ -0,0 +1,13 @@
+//
+//  FWCViewController.h
+//  CCBCheckinWebView
+//
+//  Created by Mark Petereit on 7/18/14.
+//  Copyright (c) 2014 Family Worship Center. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FWCViewController : UIViewController
+
+@end
---
+++ CCBCheckinWebView/FWCViewController.m
@@ -0,0 +1,29 @@
+//
+//  FWCViewController.m
+//  CCBCheckinWebView
+//
+//  Created by Mark Petereit on 7/18/14.
+//  Copyright (c) 2014 Family Worship Center. All rights reserved.
+//
+
+#import "FWCViewController.h"
+
+@interface FWCViewController ()
+
+@end
+
+@implementation FWCViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
