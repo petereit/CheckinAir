920792d8feb20bcdd314e1c85cecacd3aa6e7cd3
Mark Petereit <markpetereit@icloud.com>
2017-04-28 16:48:51 -0400

    Added prefersStatusBarHidden

--- CheckinWebView/FWCLaunchImageViewController.m
+++ CheckinWebView/FWCLaunchImageViewController.m
@@ -14,6 +14,11 @@
 
 @implementation FWCLaunchImageViewController
 
+-(BOOL)prefersStatusBarHidden{
+    return YES;
+}
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
